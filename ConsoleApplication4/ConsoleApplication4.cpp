78119ac122443282cdaee6faccd50493f4c5dbbf
Alex <Alex@DESKTOP-06I5DP9>
2024-06-09 19:17:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    using namespace std;
+    const int COUNT = 200;
+
+    for (int i = 1; i <= COUNT; i++) {
+        int j = i;
+        int count = 0;
+        for (j; j >= 1;) {
+            if (j == i or j==1) {
+                count++;
+                j--;
+                continue;
+            }
+            int b = i % j;
+            j--;
+            if (b == 0) {
+                continue;
+            }
+            if (b != 0) {
+                count++;
+            }
+
+            
+        }
+        if (count == (i)) {
+            cout << i << endl;
+        }
+    }
+
+}
+
